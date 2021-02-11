b2fc79261c715b5fc69582259878364fe6a86ca3
TINA MYERS <myers.tina515@gmail.com>
2021-02-10 16:33:48 -0800

    more working things

--- src/server.js
+++ src/server.js
@@ -33,9 +33,56 @@ app.use(logger);
 app.use('*', notFound);
 app.use(errorHandler);
 
-// httpServer.listen(3000, () => {
-//   console.log('go to http://localhost:3000');
-// });
+const httpServer = require('http').createServer();
+httpServer.listen(3001);
+const io = require("socket.io")(httpServer, {
+  cors: {
+    origin: "http://localhost:3002",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+  }
+});
+// const io = require('socket.io')(3001);
+// const principal = io.of('/principal');
+// curl "http://localhost:3001/socket.io/?EIO=4&transport=polling"
+// //io.attach(start);
+
+// principal.on('connection', (socket) => {
+//   console.log(' principal.on Connected: ' + socket.id);
+
+//   socket.on('pickupready', (payload) => {
+//     console.log('principal.on pickupready: ' + payload.name);
+//   });
+// })
+
+io.on('connection', (socket) => {
+  socket.on('joinRoom', ({ Student }) => {
+    socket.join(Student.teacher);
+    console.log('A user joined chatroom: ' + Student.teacher);
+  });
+  console.log('io.on Connected: ' + socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected: ' + socket.id);
+  });
+
+  socket.on('pickupready', (payload) => {
+    console.log('pickupReady: ' + payload.name);
+    socket.to(payload.teacher).emit('pickupready', payload);
+  });
+
+  socket.on('sendingstudent', (payload) => {
+    console.log('sending student out: ' + payload.name);
+    socket.emit('sendingstudent', (payload));
+  })
+
+  socket.on('leaveRoom', ({ Student }) => {
+    socket.leave(Student.teacher);
+    console.log('A user left chatroom: ' + Student.teacher);
+  });
+})
+
 
 function start(PORT) {
   app.listen(PORT, () => {
@@ -43,41 +90,6 @@ function start(PORT) {
     if (!PORT) { throw new Error('There is no port'); }
   })
 }
-
-// const io = require('socket.io')(3001);
-// const principal = io.of('/principal');
-
-// // //io.attach(start);
-
-// principal.on('connection', (socket) => {
-//   console.log('Connected: ' + socket.id);
-
-//   socket.on('pickupready', () => {
-//     console.log('what is this pickupready: ' + socket.id);
-//   });
-// })
-
-io.on('connection', (socket) => {
-  console.log('Connected: ' + socket.id);
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected: ' + socket.id);
-  });
-
-  socket.on('pickupready', (payload) => {
-    console.log(' is this the one? pickupReady: ' + payload);
-  });
-
-  socket.on('joinRoom', ({ Student }) => {
-    socket.join(Student.teacher);
-    console.log('A user joined chatroom: ' + Student.teacher);
-  });
-
-  socket.on('leaveRoom', ({ Student }) => {
-    socket.leave(Student.teacher);
-    console.log('A user left chatroom: ' + Student.teacher);
-  });
-})
 
 module.exports = {
   server: app,

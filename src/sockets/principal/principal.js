fad8ba7ccb148e8268a1bcc7766ebec94596841a
Simon Panek <paneks19@gmail.com>
2021-02-11 15:02:16 -0800

    remove all old socket files

--- src/sockets/principal/principal.js
+++ src/sockets/principal/principal.js
@@ -1,22 +1,22 @@
-'use strict';
+// 'use strict';
 
-// const coordinator = require('../coordinator/coordinator.js');
-const io = require('socket.io-client');
-//TODO: host address needed
-const host = "ws://localhost:3001/coordinator";
-const socket = io.connect(host);
+// // const coordinator = require('../coordinator/coordinator.js');
+// const io = require('socket.io-client');
+// //TODO: host address needed
+// const host = "ws://localhost:3001/coordinator";
+// const socket = io.connect(host);
 
-console.log('PRINCIPAL file is connected');
+// console.log('PRINCIPAL file is connected');
 
-socket.on('studentSentOut', updatePrincipalView);
+// socket.on('studentSentOut', updatePrincipalView);
 
-setInterval(() => {
-  socket.emit('parentReady', (payload) => {
-    console.log('PRINCIPAL', {events: 'parentReady', payload});
-  })
-}, 5000)
+// setInterval(() => {
+//   socket.emit('parentReady', (payload) => {
+//     console.log('PRINCIPAL', {events: 'parentReady', payload});
+//   })
+// }, 5000)
 
-function updatePrincipalView (payload) {
-  //TODO: use payload to update the view on the principal device
-  console.log('PRINCIPAL: updatePrincipalView ', payload);
-}
+// function updatePrincipalView (payload) {
+//   //TODO: use payload to update the view on the principal device
+//   console.log('PRINCIPAL: updatePrincipalView ', payload);
+// }
--- src/sockets/teacher/teacher.js
+++ src/sockets/teacher/teacher.js
@@ -1,33 +1,33 @@
-'use strict';
+// 'use strict';
 
-// const coordinator = require('../coordinator/coordinator.js');
-const io = require('socket.io-client');
-//TODO: host address needed
-const host = "ws://localhost:3001/coordinator";
-const socket = io.connect(host);
+// // const coordinator = require('../coordinator/coordinator.js');
+// const io = require('socket.io-client');
+// //TODO: host address needed
+// const host = "ws://localhost:3001/coordinator";
+// const socket = io.connect(host);
 
-// console.log('TEACHER file is connected');
+// // console.log('TEACHER file is connected');
 
-socket.on('connection', () => {
-  console.log('TEACHER socket.io is connected');
-})
+// socket.on('connection', () => {
+//   console.log('TEACHER socket.io is connected');
+// })
 
-let teacher = 'bob'; 
+// let teacher = 'bob'; 
 
-socket.emit('join', teacher)
+// socket.emit('join', teacher)
 
-socket.on('parentReady', (payload) => {
-  console.log('TEACHER', {events: 'parentReady', payload});
-  //TODO: change student color on teacher display
-})
+// socket.on('parentReady', (payload) => {
+//   console.log('TEACHER', {events: 'parentReady', payload});
+//   //TODO: change student color on teacher display
+// })
 
-socket.on('messageAll', (payload) => {
-  console.log('TEACHER', {events: 'messageAll', payload});
-  //TODO: display principal message at top of list
-})
+// socket.on('messageAll', (payload) => {
+//   console.log('TEACHER', {events: 'messageAll', payload});
+//   //TODO: display principal message at top of list
+// })
 
-socket.on('busRoute', (payload) => {
-  console.log('TEACHER', {events: 'busRoute', payload});
-  //TODO: change student colors to yellow based on bus route
-})
+// socket.on('busRoute', (payload) => {
+//   console.log('TEACHER', {events: 'busRoute', payload});
+//   //TODO: change student colors to yellow based on bus route
+// })
 
